146f7cd0106fe3845a12afe38c874fc3e8a82403
Marc Lainez <marc.lainez@gmail.com>
2012-06-19 14:42:47 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source :rubygems
+
+gem "dream_cheeky"
+gem "tinder"
---
+++ button.rb
@@ -0,0 +1,13 @@
+require 'dream_cheeky'
+require 'tinder'
+
+DreamCheeky::BigRedButton.run do
+  open do
+  end
+
+  close do
+  end
+
+  push do
+  end
+end
