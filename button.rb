92033093db8a8d9c76d4a046e31d62efcaf9f0ba
Marc Lainez <marc.lainez@gmail.com>
2012-06-19 14:59:36 +0200

    Add campfire auth

--- button.rb
+++ button.rb
@@ -1,13 +1,40 @@
 require 'dream_cheeky'
 require 'tinder'
+require 'yaml'
 
-DreamCheeky::BigRedButton.run do
-  open do
+class BigRedButton
+  def initialize
+    @config   = load_campfire_config
+    @campfire = campfire_authenticate
   end
 
-  close do
+  def load_campfire_config
+    YAML::load(File.open('config.yml'))["campfire"]
   end
 
-  push do
+  def campfire_authenticate
+    api_token = @config["token"]
+    subdomain = @config["subdomain"]
+    Tinder::Campfire.new subdomain, :token => api_token
+  end
+
+  def room
+    @campfire.room
+  end
+
+  def start_loop
+    DreamCheeky::BigRedButton.run do
+      open do
+      end
+
+      close do
+      end
+
+      push do
+      end
+    end
   end
 end
+
+button = BigRedButton.new
+button.start_loop
